c35013d006d7b4742eb1a618f6d6c2d02f71d03c
nccstar082 <70097757@qq.com>
2025-05-28 13:02:18 +0800

    修改connection_page.dart

--- src/tray.rs
+++ src/tray.rs
@@ -53,13 +53,12 @@ fn make_tray() -> hbb_common::ResultType<()> {
     let icon = tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height)
         .context("Failed to open icon")?;
 
-// 创建事件循环和菜单
-let mut event_loop = EventLoopBuilder::new().build();
-let tray_menu = Menu::new();
-// 修改菜单项名称为“退出”
-let quit_i = MenuItem::new(translate("退出".to_owned()), true, None);
-let open_i = MenuItem::new(translate("Open".to_owned()), true, None);
-tray_menu.append_items(&[&open_i, &quit_i]).ok();
+    let mut event_loop = EventLoopBuilder::new().build();
+
+    let tray_menu = Menu::new();
+    let quit_i = MenuItem::new(translate("Stop service".to_owned()), true, None);
+    let open_i = MenuItem::new(translate("Open".to_owned()), true, None);
+    tray_menu.append_items(&[&open_i, &quit_i]).ok();
     let tooltip = |count: usize| {
         if count == 0 {
             format!(
@@ -118,73 +117,90 @@ tray_menu.append_items(&[&open_i, &quit_i]).ok();
         use tao::platform::macos::EventLoopExtMacOS;
         event_loop.set_activation_policy(tao::platform::macos::ActivationPolicy::Accessory);
     }
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::WaitUntil(
-        std::time::Instant::now() + std::time::Duration::from_millis(100),
-    );
-    if let tao::event::Event::NewEvents(tao::event::StartCause::Init) = event {
-        let tray = TrayIconBuilder::new()
-           .with_menu(Box::new(tray_menu.clone()))
-           .with_tooltip(tooltip(0))
-           .with_icon(icon.clone())
-           .with_icon_as_template(true) // mac only
-           .build();
-        match tray {
-            Ok(tray) => _tray_icon = Arc::new(Mutex::new(Some(tray))),
-            Err(err) => {
-                log::error!("Failed to create tray icon: {}", err);
-            }
-        };
-        #[cfg(target_os = "macos")]
-        unsafe {
-            use core_foundation::runloop::{CFRunLoopGetMain, CFRunLoopWakeUp};
-            let rl = CFRunLoopGetMain();
-            CFRunLoopWakeUp(rl);
-        }
-    }
-    if let Ok(event) = menu_channel.try_recv() {
-        if event.id == quit_i.id() {
-            // 修改为直接退出程序，不卸载服务
-            *control_flow = ControlFlow::Exit;
-        } else if event.id == open_i.id() {
-            open_func();
-        }
-    }
-    if let Ok(_event) = tray_channel.try_recv() {
-        #[cfg(target_os = "windows")]
-        match _event {
-            TrayEvent::Click {
-                button,
-                button_state,
-                ..
-            } => {
-                if button == tray_icon::MouseButton::Left
-                    && button_state == tray_icon::MouseButtonState::Up
-                {
-                    if last_click.elapsed() < std::time::Duration::from_secs(1) {
-                        return;
-                    }
-                    open_func();
-                    last_click = std::time::Instant::now();
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::WaitUntil(
+            std::time::Instant::now() + std::time::Duration::from_millis(100),
+        );
+
+        if let tao::event::Event::NewEvents(tao::event::StartCause::Init) = event {
+            // We create the icon once the event loop is actually running
+            // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
+            let tray = TrayIconBuilder::new()
+                .with_menu(Box::new(tray_menu.clone()))
+                .with_tooltip(tooltip(0))
+                .with_icon(icon.clone())
+                .with_icon_as_template(true) // mac only
+                .build();
+            match tray {
+                Ok(tray) => _tray_icon = Arc::new(Mutex::new(Some(tray))),
+                Err(err) => {
+                    log::error!("Failed to create tray icon: {}", err);
                 }
+            };
+
+            // We have to request a redraw here to have the icon actually show up.
+            // Tao only exposes a redraw method on the Window so we use core-foundation directly.
+            #[cfg(target_os = "macos")]
+            unsafe {
+                use core_foundation::runloop::{CFRunLoopGetMain, CFRunLoopWakeUp};
+
+                let rl = CFRunLoopGetMain();
+                CFRunLoopWakeUp(rl);
             }
-            _ => {}
         }
-    }
-    #[cfg(windows)]
-    if let Ok(data) = ipc_receiver.try_recv() {
-        match data {
-            Data::ControlledSessionCount(count) => {
-                _tray_icon
-                   .lock()
-                   .unwrap()
-                   .as_mut()
-                   .map(|t| t.set_tooltip(Some(tooltip(count))));
+
+        if let Ok(event) = menu_channel.try_recv() {
+            if event.id == quit_i.id() {
+                /* failed in windows, seems no permission to check system process
+                if !crate::check_process("--server", false) {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                */
+                if !crate::platform::uninstall_service(false, false) {
+                    *control_flow = ControlFlow::Exit;
+                }
+            } else if event.id == open_i.id() {
+                open_func();
             }
-            _ => {}
         }
-    }
-});
+
+        if let Ok(_event) = tray_channel.try_recv() {
+            #[cfg(target_os = "windows")]
+            match _event {
+                TrayEvent::Click {
+                    button,
+                    button_state,
+                    ..
+                } => {
+                    if button == tray_icon::MouseButton::Left
+                        && button_state == tray_icon::MouseButtonState::Up
+                    {
+                        if last_click.elapsed() < std::time::Duration::from_secs(1) {
+                            return;
+                        }
+                        open_func();
+                        last_click = std::time::Instant::now();
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        #[cfg(windows)]
+        if let Ok(data) = ipc_receiver.try_recv() {
+            match data {
+                Data::ControlledSessionCount(count) => {
+                    _tray_icon
+                        .lock()
+                        .unwrap()
+                        .as_mut()
+                        .map(|t| t.set_tooltip(Some(tooltip(count))));
+                }
+                _ => {}
+            }
+        }
+    });
 }
 
 #[cfg(windows)]
